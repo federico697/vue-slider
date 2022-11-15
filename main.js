591d05369ae343141330fb73e70a50f64d9c71a4
federico697 <federico697697@gmail.com>
2022-11-15 12:35:28 +0100

    correzione esercizio

--- main.js
+++ main.js
@@ -7,25 +7,60 @@ nello slider ci sono dei pallini in basso al click dei pallini cambia l'immagine
 a ogni 3 sec l'immagine cambia automaticamente senza nessun click da parte nostra
 */
 
-var root = new Vue({
-    
-    el:'#root',
+var app = new Vue({
+    el: '#app',
     data: {
-        album: ["./img/img1.jpg", "./img/img2.jpg"],
         indice: 0,
+        obj: [
+            {
+                url: './img/01.png',
+                alt: 'descrizione immagine 1'
+            },
+            {
+                url: './img/02.png',
+                alt: 'descrizione immagine 2'
+            },
+            {
+                url: './img/03.png',
+                alt: 'descrizione immagine 3'
+            },
+            {
+                url: './img/04.png',
+                alt: 'descrizione immagine 4'
+            },
+            {
+                url: './img/05.png',
+                alt: 'descrizione immagine 5'
+            },
+        ],
+    },
+    mounted() {
+        // entra quando l'applicativo vue ha caricato el: #app
+        this.attivaIntervallo()
     },
     methods: {
-        cambiaOggettoRight: function(){
-            this.indice++;
-            if( this.indice > this.album.length - 1){
-                return this.indice = 0
+        immagineSuccessiva(){
+            // cambio immagine in avanti
+            // controllo che l'indice sia minore della lunghezza dell'oggetto
+            if( this.indice < this.obj.length - 1 ){
+                this.indice++;
+            } else{
+                this.indice = 0;
             }
         },
-        cambiaOggettoLeft: function(){
-            this.indice++;
-            if( this.indice > this.album.length - 1){
-                return this.indice = 0
+        immaginePrecedente(){
+            // se l'indice è minore di 0 torna all'ultimno elemento
+            if( this.indice <= 0 ){
+                this.indice = this.obj.length -1;
+            } else{
+                this.indice--;
             }
+        },
+        attivaIntervallo(){
+            setInterval( ()=> { this.immagineSuccessiva() }, 3000 );
+        },
+        selezioneImmagine(index){
+            this.indice = index;
         }
     }
-});
+})
